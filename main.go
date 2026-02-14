19dfa5b2c1478a4f075cf1b88f23afc1c9ade2ad
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-02-14 20:42:47 +0530

    wire db Init into main

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "task-manager/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"task/cmd"
+	"task/db"
+
+	"github.com/mitchellh/go-homedir"
+)
 
 func main() {
-	cmd.RootCmd.Execute() // rootcmd is the TOP LEVEL
+
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute()) // rootcmd is the TOP LEVEL
+}
+
+// wrapper func to handle errors.
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
